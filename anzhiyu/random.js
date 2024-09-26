682446dd5b213e666a359a800550e64be27ac6e0
jdh <3219591492@qq.com>
2024-09-26 20:47:27 +0800

    Site updated: 2024-09-26 20:47:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/14/c-程序与输出/","2124/09/10/hello-world/","2024/09/13/军训/","2024/09/24/热点分析-网络带货/"];function toRandomPost(){
+var posts=["2124/09/10/hello-world/","2024/09/13/军训/","2024/09/14/c-程序与输出/","2024/09/24/热点分析-网络带货/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
